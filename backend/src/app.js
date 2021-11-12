401716aadc20198e51790a89de503893805f146e
acyadigar <alpercanyadigar@gmail.com>
2021-11-12 14:43:36 +0300

    Implement helmet

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import './mongo-connection'
 import express from 'express';
 import Boom from 'boom';
 import cors from 'cors';
+import helmet from 'helmet'
 
 import routes from './routes'
 
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes)
+app.use(helmet())
 
 app.use((req, res, next) => {
   return next(Boom.notFound('This route does not exist.'));
