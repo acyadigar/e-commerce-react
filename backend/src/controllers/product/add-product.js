3e822fc9775db70939f20fc27bc2eb120bb03584
acyadigar <alpercanyadigar@gmail.com>
2022-05-03 22:18:03 +0300

    Edit checking decoded jwt and existing product

--- backend/src/controllers/product/add-product.js
+++ backend/src/controllers/product/add-product.js
@@ -3,12 +3,15 @@ import ProductService from "../../services/product-service"
 import ProductSchema from './validations'
 
 const addProduct = async (req, res, next) => {
-  if (!req.payload.role === 'user') {
+  if (req.payload._doc.role === 'user') {
     return next(Boom.unauthorized('Unauthorized'))
   }
   
   const { error } = ProductSchema.validate(req.body)
   if(error) return next(Boom.badRequest('Bad Request!'))
+
+  const isExist = await ProductService.findName(req.body.name)
+  if(isExist) return next(Boom.conflict('Already Exists!'))
 
   const product = await ProductService.add(req.body)
   res.send(product)
