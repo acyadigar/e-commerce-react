f5148e78d323d45f61903d04baf0e74dfb838340
acyadigar <alpercanyadigar@gmail.com>
2022-05-03 22:16:23 +0300

    Edit user registeration on api

--- backend/src/controllers/auth/register.js
+++ backend/src/controllers/auth/register.js
@@ -1,17 +1,20 @@
-import Boom from 'boom'
-import UserService from '../../services/user-service'
-import UserSchema from './validations'
+import Boom from "boom";
+import UserService from "../../services/user-service";
+import UserSchema from "./validations";
 
 const register = async (req, res, next) => {
-  const { error } = UserSchema.validate(req.body)
-  if (error) return next(Boom.badRequest('Bad request!'))
-  
+  const { error } = UserSchema.validate(req.body);
+  if (error) return next(Boom.badRequest("Bad request!"));
+
   if (await UserService.findUser(req.body.email)) {
-    return next(Boom.conflict('The email already registered!'))
+    return next(Boom.conflict("The email already registered!"));
   }
 
-  const user = await UserService.register(req.body)
-  res.send(user)
-}
+  const createdUser = await UserService.register(req.body);
+  const token = await UserService.signToken(createdUser)
+  const user = { user: createdUser, token }
+  
+  res.send(user);
+};
 
-export default register
+export default register;
