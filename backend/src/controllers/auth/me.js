59710e7628fe5f1288c477be706690ccda9ac8ec
acyadigar <alpercanyadigar@gmail.com>
2022-05-04 13:15:16 +0300

    Add middleware to fetching user route

--- backend/src/controllers/auth/me.js
+++ backend/src/controllers/auth/me.js
@@ -1,10 +1,7 @@
-import Boom from "boom"
 import UserService from "../../services/user-service"
 
-const me = async (req, res, next) => {
-  const decodedToken = await UserService.verifyToken(req.body.token)
-  if (!decodedToken) return next(Boom.unauthorized("Invalid token"))
-
+const me = async (req, res) => {
+  const decodedToken = req.payload
   const userMail = decodedToken._doc.email
   const decodedUser = await UserService.findUser(userMail)
   const token = await UserService.signToken(decodedUser)
--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -5,9 +5,10 @@ import users from '../controllers/auth/users'
 import register from '../controllers/auth/register'
 import login from '../controllers/auth/login'
 import me from '../controllers/auth/me'
+import verifyToken from '../middlewares/verify-token'
 
 router.get('/users', users)
-router.post('/me', me)
+router.post('/me', verifyToken, me)
 router.post('/register', register)
 router.post('/login', login)
 
