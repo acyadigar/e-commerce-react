42e1687b9bf8d8efa3ee2fdce006093134cbb2eb
acyadigar <alpercanyadigar@gmail.com>
2022-05-17 15:36:51 +0300

    Add header to axios

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
+const token = localStorage.getItem('token')
 
 const http = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "http://localhost:4000/",
+  headers: {
+    'Authorization': `Bearer ${token}`
+  }
 });
 
 export default http;
